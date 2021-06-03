1641c21412419862d85cf6f087a3b3147643a3c0
Rahul Kumar <rahul.kumar14@stryker.com>
2021-06-03 21:29:55 +0530

    Add project files.

---
+++ HeapDataStructure/Helper/MaxHeap.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapDataStructure.Helper
+{
+    internal class MaxHeap
+    {
+        private int top = 0;
+        private int[] Items { get; set; }
+
+        public MaxHeap(int size)
+        {
+            Items = new int[size];
+        }
+
+        internal void CreateHeap()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num == -1)
+                {
+                    break;
+                }
+
+                InsertNode(num);
+            }
+
+            DisplayHeap();
+        }
+
+        internal void InsertNode(int num)
+        {
+            if(top >= Items.Length - 1)
+            {
+                Console.WriteLine("No space available");
+                return;
+            }
+
+            int i = ++top;
+
+            while(i > 1 && Items[i / 2] < num)
+            {
+                Items[i] = Items[i / 2];
+                i = i / 2;
+            }
+
+            Items[i] = num;
+        }
+
+        internal void DisplayHeap()
+        {
+            for(int i = 1; i <= top; i++)
+            {
+                Console.Write(Items[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        internal void DeleteNode()
+        {
+            Items[1] = Items[top];
+            Items[top--] = 0;
+
+            int i = 1, j = i * 2;
+
+            while (j <= top)
+            {
+                if (j < top && Items[j + 1] > Items[j])
+                {
+                    j = j + 1;
+                }
+
+                if(Items[j] > Items[i])
+                {
+                    int temp = Items[i];
+                    Items[i] = Items[j];
+                    Items[j] = temp;
+
+                    i = j;
+                    j = 2 * j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            DisplayHeap();
+        }
+
+        internal void HeapifyCreate(int[] items, int top)
+        {
+            for(int i = top / 2; i > 0; i--)
+            {
+                int j = i;
+                int K = i * 2;
+
+                while (K <= top)
+                {
+                    if (K < top && items[K + 1] > items[K])
+                    {
+                        K = K + 1;
+                    }
+
+                    if (K <= top && items[K] > items[j])
+                    {
+                        int temp = items[j];
+                        items[j] = items[K];
+                        items[K] = temp;
+
+                        j = K;
+                        K = j * 2;
+                    }
+                    else
+                        break;
+                }
+            }
+
+            Items = items;
+            this.top = top;
+
+            DisplayHeap();
+        }
+    }
+}
---
+++ HeapDataStructure/Program.cs
@@ -0,0 +1,63 @@
+﻿using HeapDataStructure.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxHeap maxHeap = new MaxHeap(10);
+
+            while (true)
+            {
+                Console.WriteLine
+                    ("Please select an option" +
+                        Environment.NewLine + "1. Create Heap" +
+                        Environment.NewLine + "2. Insert Node" +
+                        Environment.NewLine + "3. Delete Node" +
+                        Environment.NewLine + "4. Create Heap - Heapify Method" +
+                        Environment.NewLine + "0. Exit"
+                    );
+
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine(Environment.NewLine + "Input format is not valid. Please try again." + Environment.NewLine);
+                }
+
+                if (i == 0)
+                {
+                    Environment.Exit(0);
+                }
+                else if (i == 1)
+                {
+                    maxHeap.CreateHeap();
+
+                }
+                else if (i == 2)
+                {
+                    //tree.InsertNode();
+                }
+                else if (i == 3)
+                {
+                    maxHeap.DeleteNode();
+                }
+                else if (i == 4)
+                {
+                    Console.WriteLine("Enter the numbers{comma separated)"); //30,15,35,40,10,12,6,5,20
+                    int[] items = ("0, " + Console.ReadLine()).Split(',').Select(x => Convert.ToInt32(x.Trim())).ToArray();
+
+                    maxHeap.HeapifyCreate(items, items.Length - 1);
+                }
+                else
+                {
+                    Console.WriteLine("Please select a valid option.");
+                }
+            }
+        }
+    }
+}
